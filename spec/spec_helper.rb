c1d6db4087d854f71bbb19da6ea62264239b0283
Jake Levine <jake@drunkonvinyl.com>
2011-12-12 12:59:56 -0800

    added new html for about contact home

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,18 @@
-require 'rubygems'
 require 'spork'
+require 'rubygems'
 
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However, 
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../../config/routes", __FILE__)
+require 'rspec/rails'
+
+#Requires supporting files with custom matchers and macros, etc,
+#in ./support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}  
 end
 
 Spork.each_run do
@@ -54,3 +61,4 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 end
+
